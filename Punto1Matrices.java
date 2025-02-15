2643efecc1212c955ba133de790b676eb254ea76
J-Moren <kalev.1503@gmail.com>
2025-02-15 16:12:26 -0500

    Se carga Clase principal Punto 1 y se carga Metodos de dimensión Matriz y llenado Matriz

---
+++ Punto1Matrices.java
@@ -0,0 +1,15 @@
+//Realice un algoritmo que almacene números en una matriz de 6 * 6. Imprimir la suma de los números almacenados en la matriz
+
+
+
+public class Punto1Matrices {
+
+        public static void main (String[] args) {
+            metodos m = new metodos();
+            int d = m.solicitarDimension();
+
+
+        }
+
+    
+}
